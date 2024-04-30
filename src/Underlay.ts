3e85ed49ac694ccc50dad966b175776267c6a7e4
Orion Reed <orionreedt@gmail.com>
2024-04-30 21:24:20 +0200

    good sand (checkpoint)

--- src/Underlay.ts
+++ src/Underlay.ts
@@ -8,15 +8,15 @@ export class Underlay {
   height: number
   buffer: p5.Graphics | null = null
   cellSize = 10;
-  gridSize = 100;
+  worldSize = 100;
   particles: Particle[] = [];
-  grid: (Particle | null)[];
+  world: (Particle | null)[];
 
   constructor(editor: Editor) {
     this.editor = editor
     this.width = window.innerWidth;
     this.height = window.innerHeight;
-    this.grid = new Array(this.gridSize * this.gridSize).fill(null);
+    this.world = new Array(this.worldSize * this.worldSize).fill(null);
 
     this.editor.createShape({
       id: createShapeId(),
@@ -33,12 +33,16 @@ export class Underlay {
         sketch.createCanvas(this.width, this.height);
         this.buffer = sketch.createGraphics(this.width, this.height);
 
-        for (let i = 0; i < 100; i++) {
-          let x = Math.floor(sketch.random(this.gridSize));
-          let y = Math.floor(sketch.random(this.gridSize));
-          let sand = new Sand(sketch, x, y, sketch.color(194, 178, 128), this.cellSize, this.grid);
+        for (let i = 0; i < 500; i++) {
+          const x = Math.floor(sketch.random(this.worldSize));
+          const y = Math.floor(sketch.random(this.worldSize));
+          const sand = new Sand(sketch, x, y, this.worldSize, this.world);
           this.particles.push(sand);
-          this.grid[y * this.cellSize + x] = sand;
+          this.world[y * this.worldSize + x] = sand;
+        }
+        this.editor.store.onAfterChange = (prev, next, source) => {
+          if (next.typeName !== 'shape') return;
+          this.setShapesToStone()
         }
       };
       sketch.draw = () => {
@@ -53,21 +57,26 @@ export class Underlay {
         this.buffer.scale(cam.z);
         this.buffer.translate(cam.x, cam.y);
 
-        // for (let i = 0; i < 100; i++) {
-        //   for (let j = 0; j < 100; j++) {
-        //     this.buffer.fill(this.p5.color(this.p5.random(255), this.p5.random(255), this.p5.random(255)));
-        //     this.buffer.square(i * this.gridSize, j * this.gridSize, this.gridSize);
-        //   }
-        // }
+        // Check if mouse is down and add particles
+        if (this.editor.inputs.isPointing) {
+          // const sand = new Sand ()
+          if (this.editor.inputs.altKey) {
+            this.addParticleAtPointer(sketch.mouseX, sketch.mouseY, Stone)
+          }
+          else {
+            this.addParticleAtPointer(sketch.mouseX, sketch.mouseY, Sand)
+          }
+        }
+
         // Update particles
         for (const particle of this.particles) {
           particle.update();
         }
 
         // Draw particles
-        this.buffer.fill('orange');
         this.buffer.noStroke();
         for (const particle of this.particles) {
+          this.buffer.fill(particle.color);
           this.buffer.rect(particle.position.x * this.cellSize, particle.position.y * this.cellSize, this.cellSize, this.cellSize);
         }
         this.buffer.pop();
@@ -75,17 +84,46 @@ export class Underlay {
       };
     });
   }
+
+  setShapesToStone() {
+    const shapes = this.editor.getCurrentPageShapes();
+    for (const shape of shapes) {
+      // this.editor.setShapeProp(shape.id, 'fill', 'stone');
+    }
+  }
+
+  addParticleAtPointer<T extends Particle>(x: number, y: number, particle: new (...args: any[]) => T) {
+    const cam = this.editor.getCamera();
+    const transformedX = (x - cam.x) / cam.z;
+    const transformedY = (y - cam.y) / cam.z;
+    const numParticles = 10;
+    const radius = 10;
+
+    for (let i = 0; i < numParticles; i++) {
+      const angle = (i / numParticles) * 2 * Math.PI;
+      const particleX = transformedX + radius * Math.cos(angle);
+      const particleY = transformedY + radius * Math.sin(angle);
+      const gridX = Math.floor(particleX / this.cellSize);
+      const gridY = Math.floor(particleY / this.cellSize);
+      const index = gridY * this.worldSize + gridX;
+
+      if (gridX >= 0 && gridX < this.worldSize && gridY >= 0 && gridY < this.worldSize && !this.world[index]) {
+        const p = new particle(this.p5, gridX, gridY, this.worldSize, this.world)
+        this.particles.push(p);
+        this.world[index] = p;
+      }
+    }
+  }
 }
 
 abstract class Particle {
   position: p5.Vector;
-  color: p5.Color;
   gridSize: number;
   grid: (Particle | null)[];
+  abstract color: p5.Color;
 
-  constructor(public p5: p5, x: number, y: number, color: p5.Color, gridSize: number, grid: (Particle | null)[]) {
+  constructor(public p5: p5, x: number, y: number, gridSize: number, grid: (Particle | null)[]) {
     this.position = p5.createVector(x, y);
-    this.color = color;
     this.gridSize = gridSize;
     this.grid = grid;
   }
@@ -94,6 +132,7 @@ abstract class Particle {
 }
 
 class Sand extends Particle {
+  color = this.p5.color(this.p5.random(220, 240), this.p5.random(170, 180), this.p5.random(50, 70));
   update() {
     const x = this.position.x;
     const y = this.position.y;
@@ -101,14 +140,33 @@ class Sand extends Particle {
     const belowRight = (y + 1) * this.gridSize + (x + 1);
     const belowLeft = (y + 1) * this.gridSize + (x - 1);
 
-    if (y + 1 < this.gridSize && !this.grid[below]) {
-      this.position.y += 1;
-    } else if (y + 1 < this.gridSize && x + 1 < this.gridSize && !this.grid[belowRight]) {
-      this.position.x += 1;
-      this.position.y += 1;
-    } else if (y + 1 < this.gridSize && x - 1 >= 0 && !this.grid[belowLeft]) {
-      this.position.x -= 1;
-      this.position.y += 1;
+    // Check and move to the new position if possible
+    if (y + 1 < this.gridSize && this.canMoveTo(below)) {
+      this.moveParticle(x, y + 1, below);
+    } else if (y + 1 < this.gridSize && x + 1 < this.gridSize && this.canMoveTo(belowRight)) {
+      this.moveParticle(x + 1, y + 1, belowRight);
+    } else if (y + 1 < this.gridSize && x - 1 >= 0 && this.canMoveTo(belowLeft)) {
+      this.moveParticle(x - 1, y + 1, belowLeft);
     }
+  }
+
+  canMoveTo(index: number): boolean {
+    return !this.grid[index]
+  }
+
+  moveParticle(newX: number, newY: number, newIndex: number) {
+    // Update grid references
+    this.grid[this.position.y * this.gridSize + this.position.x] = null;
+    this.position.x = newX;
+    this.position.y = newY;
+    this.grid[newIndex] = this;
+  }
+}
+
+class Stone extends Particle {
+  color = this.p5.color('grey');
+  // Override the update method to prevent movement
+  update() {
+    // Stone does not move, so this method is intentionally left empty
   }
 }
