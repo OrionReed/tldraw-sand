cd1b19c16535dfe0bb24bd6a5ba6c8b6db7cd0d3
Orion Reed <orionreedt@gmail.com>
2024-05-01 16:24:40 +0100

    ui for sand type

--- src/ui/SandTool.tsx
+++ src/ui/SandTool.tsx
@@ -1,6 +1,16 @@
 import { StateNode } from 'tldraw'
 
+class Sand extends StateNode { static override id = 'sand' }
+class Stone extends StateNode { static override id = 'stone' }
+class Air extends StateNode { static override id = 'air' }
+
 export class SandTool extends StateNode {
   static override id = 'sand'
-  static override initial = 'idle'
+  static override initial = 'sand'
+  static override children = () => [Sand, Stone, Air]
+
+  override onEnter = () => {
+    this.editor.setCursor({ type: 'cross' })
+  }
 }
+
--- src/ui/components.tsx
+++ src/ui/components.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   DefaultToolbar,
   TLComponents,
@@ -5,9 +6,59 @@ import {
   useIsToolSelected,
   DefaultToolbarContent,
   useTools,
+  DefaultStylePanel,
+  DefaultStylePanelContent,
+  TLUiStylePanelProps,
+  TldrawUiButton,
+  TldrawUiButtonLabel,
+  useRelevantStyles,
+  useEditor
 } from "tldraw";
 
+function DefaultPanel(props: TLUiStylePanelProps) {
+  return <DefaultStylePanel {...props} />
+}
+
+
+function CustomStylePanel(props: TLUiStylePanelProps) {
+  const tools = useTools()
+  const isSandSelected = useIsToolSelected(tools.sand)
+  const editor = useEditor()
+  const currentTool = editor.getCurrentTool()
+  const styles = useRelevantStyles()
+  const [currentPath, setCurrentPath] = useState(editor.getPath())
+
+  function SandButton({ name }: { name: string }) {
+    const path = `sand.${name}`
+    const isSelected = editor.getPath() === path
+    return <TldrawUiButton
+      type="normal"
+      onClick={() => {
+        editor.setCurrentTool(path)
+        setCurrentPath(path)
+      }}
+      style={{ color: isSelected ? 'green' : 'black' }}>
+      <TldrawUiButtonLabel>{capitalizeFirstLetter(name)}</TldrawUiButtonLabel>
+    </TldrawUiButton>
+  }
+  return (
+    <DefaultStylePanel {...props}>
+      {isSandSelected && currentTool.children && (
+        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '2px' }}>
+          {Object.keys(currentTool.children).map((key) => {
+            console.log('mapping')
+            return <SandButton key={key} name={key} />
+          })
+          }
+        </div>
+      )}
+      <DefaultStylePanelContent styles={styles} />
+    </DefaultStylePanel>
+  )
+}
+
 export const uiComponents: TLComponents = {
+  StylePanel: CustomStylePanel,
   Toolbar: (props) => {
     const tools = useTools()
     const isSandSelected = useIsToolSelected(tools.sand)
@@ -18,4 +69,8 @@ export const uiComponents: TLComponents = {
       </DefaultToolbar>
     )
   }
+}
+
+function capitalizeFirstLetter(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
